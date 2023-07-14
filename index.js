ef46ca38d9649f8b77402a5aa6a58069b5039fac
Ankit <rankit2883@gmail.com>
2023-07-14 22:13:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("hello");
+const el = document.querySelector(".bold");
+// console.log(el);
+// console.log(el.document.getAttribute("class"))
+const cls =el.getAttribute("class");
+console.log(cls)
